aa97e8d94f9ccfc41eec740e58838f1bb0e0e3de
kamil <kamil.blazewicz2810@gmail.com>
2023-06-14 21:08:22 +0200

    AES-68 created api for organizations list

---
+++ src/main/kotlin/pl/edu/agh/server/organization/OrganizationService.kt
@@ -0,0 +1,24 @@
+package pl.edu.agh.server.organization
+
+import org.springframework.stereotype.Service
+
+@Service
+class OrganizationService(private val organizationRepository: OrganizationRepository) {
+
+  fun createOrganization(organization: Organization): Organization =
+      organizationRepository.save(organization)
+
+  fun updateOrganization(id: Long, organization: Organization): Organization? =
+      organizationRepository.findById(id)
+          .map { existingOrganization ->
+            val updatedOrganization = existingOrganization.copy(id = id)
+            organizationRepository.save(updatedOrganization)
+          }
+          .orElse(null)
+
+  fun getAllOrganizations(): List<Organization> =
+      organizationRepository.findAll()
+
+  fun getOrganization(id: Long): Organization? =
+      organizationRepository.findById(id).orElse(null)
+}
