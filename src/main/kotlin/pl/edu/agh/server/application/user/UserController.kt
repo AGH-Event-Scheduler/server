e65b8036c0d6f8a49015ac61c183b7f90d1cec8d
fkitka <filip5845@gmail.com>
2023-11-20 00:53:20 +0100

    AES-105 add get user

--- src/main/kotlin/pl/edu/agh/server/application/user/UserController.kt
+++ src/main/kotlin/pl/edu/agh/server/application/user/UserController.kt
@@ -1,11 +1,26 @@
 package pl.edu.agh.server.application.user
 
+import org.modelmapper.ModelMapper
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import pl.edu.agh.server.domain.dto.UserDTO
+import pl.edu.agh.server.domain.user.User
 import pl.edu.agh.server.domain.user.UserRepository
+import pl.edu.agh.server.domain.user.UserService
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/users/")
 class UserController(
-    private val userRepository: UserRepository,
-)
+    private val userService: UserService,
+    private val modelMapper: ModelMapper
+) {
+    @GetMapping
+    fun getUser(
+        @RequestParam(name = "email") email: String,
+    ): UserDTO {
+        val user = userService.getUserByEmail(email)
+        return modelMapper.map(user, UserDTO::class.java)
+    }
+}
---
+++ src/main/kotlin/pl/edu/agh/server/domain/dto/UserDTO.kt
@@ -0,0 +1,9 @@
+package pl.edu.agh.server.domain.dto
+
+import java.util.*
+
+data class UserDTO(
+    var email: String? = null,
+    var name: String? = null,
+    var lastname: String? = null,
+)
