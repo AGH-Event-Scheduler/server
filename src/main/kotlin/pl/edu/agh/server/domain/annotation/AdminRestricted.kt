7ecde50215c0353e07d91868638f9edc5dec25f1
kamil <kamil.blazewicz2810@gmail.com>
2023-12-09 16:31:46 +0100

    [feat] AES-89 refactor

--- src/main/kotlin/pl/edu/agh/server/domain/annotation/AdminRestricted.kt
+++ src/main/kotlin/pl/edu/agh/server/domain/annotation/AdminRestricted.kt
@@ -3,9 +3,7 @@ package pl.edu.agh.server.domain.annotation
 /**
  * Annotation to specify access control for a method.
  *
- * @param allowedRoles The roles allowed to access the method. (passed as array of strings)
- *
- * annotation requires request: HttpServletRequest, @PathVariable organizationId/eventId: Long to be passed as first two arguments
+ * annotation requires request: HttpServletRequest
  *
  */
 @Retention(AnnotationRetention.RUNTIME)
